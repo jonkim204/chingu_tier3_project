364f83c3687bab8b74c0dc16e0a848527f26be8b
Jonathan Kim <jonkim204@gmail.com>
2019-05-22 13:53:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'fixed-data-table-2/dist/fixed-data-table.css';
+import { Table, Column, Cell } from 'fixed-data-table-2';
+import getData from './getData';
 
 function App() {
+  const data = getData();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        Meteorite Explorer
       </header>
+      <div className="search-box">
+        <input type="text" id="search-input"></input>
+        <input type="button" id="search-button" value="SEARCH"></input>
+      </div>
+      <div>
+      <Table
+        rowHeight={50}
+        rowsCount={data.length}
+        headerHeight={50}
+        width={1000}
+        height={500}
+      >
+        <Column
+          columnKey="firstName"
+          header={<Cell>First Name</Cell>}
+          width={130}
+          cell={({ rowIndex, columnKey }) => {
+            return <Cell>{data[rowIndex][columnKey]}</Cell>;
+          }}
+        />
+        <Column
+          columnKey="lastName"
+          header={<Cell>Last Name</Cell>}
+          width={130}
+          cell={({ rowIndex, columnKey }) => {
+            return <Cell>{data[rowIndex][columnKey]}</Cell>;
+          }}
+        />
+        <Column
+          columnKey="email"
+          header={<Cell>Email</Cell>}
+          width={320}
+          cell={({ rowIndex, columnKey }) => {
+            return <Cell>{data[rowIndex][columnKey]}</Cell>;
+          }}
+        />
+        <Column
+          columnKey="city"
+          header={<Cell>City</Cell>}
+          width={180}
+          cell={({ rowIndex, columnKey }) => {
+            return <Cell>{data[rowIndex][columnKey]}</Cell>;
+          }}
+        />
+        <Column
+          columnKey="salary"
+          header={<Cell>Salary</Cell>}
+          width={180}
+          cell={({ rowIndex, columnKey }) => {
+            return <Cell>{data[rowIndex][columnKey]}</Cell>;
+          }}
+        />
+      </Table>
+      </div>
     </div>
   );
 }
---
+++ src/getData.js
@@ -0,0 +1,19 @@
+import faker from 'faker';
+
+const createFakeRowData = () => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  city: faker.address.city(),
+  salary: faker.random
+    .number({
+      min: 50000,
+      max: 500000
+    })
+    .toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+});
+
+export default () => Array.from({ length: 2000 }, createFakeRowData);
